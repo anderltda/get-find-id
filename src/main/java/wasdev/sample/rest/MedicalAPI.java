7a37c225536f59b10682f2e253c18ebf7039d44d
anderson.nascimento <anderson.nascimento@D20150100044.odontoempresas.intranet>
2018-12-14 12:19:10 -0200

    add project

---
+++ src/main/java/wasdev/sample/rest/MedicalAPI.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package wasdev.sample.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+import wasdev.sample.store.MedicalStore;
+import wasdev.sample.store.MedicalStoreFactory;
+import wasdev.sample.vo.Medical;
+
+/**
+ * @author anderson.nascimento
+ *
+ */
+@ApplicationPath("api")
+@Path("/medical")
+public class MedicalAPI extends Application {
+
+	//Our database store
+	MedicalStore store = MedicalStoreFactory.getInstance();
+
+    /**
+     * @return
+     */
+    @POST
+    @Path("/create")
+    @Produces("application/text")
+    @Consumes("application/json")
+    public String newToDo(Medical medical) {
+      if(store == null) {
+    	  return String.format("No found store", medical.getName());
+      }
+      store.persist(medical);
+      return String.format("O Médico %s foi inserido com sucesso.", medical.getName());
+    }
+}
